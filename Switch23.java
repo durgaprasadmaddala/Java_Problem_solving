9fbeaf7a186c760e5dacedb7da6a81e33339edf8
MADDALA DURGAPRASAD <durgaprasadmaddalaa@gmail.com>
2026-09-08 13:48:02 +0530

    Add files via upload

---
+++ Switch23.java
@@ -0,0 +1,35 @@
+package Ifelsestatements;
+
+import java.util.Scanner;
+
+public class Switch23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Select your food");
+		System.out.println("1.idly");
+
+		System.out.println("2. poori");
+
+		System.out.println("3. dosa");
+
+		Scanner sc =new Scanner(System.in);
+		
+		
+		
+		System.out.println("your number");
+		String st=sc.next();
+		System.out.println(st);
+		
+		switch(st) {
+		
+		case dosa 1->{
+			System .out.println("dosa");
+		}
+		case 
+		}
+		
+	}
+
+}
