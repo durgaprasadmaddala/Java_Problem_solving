e33d62e49b96688f6fad0e8abbcd5d30d1fcfaac
MADDALA DURGAPRASAD <durgaprasadmaddalaa@gmail.com>
2026-09-08 13:48:23 +0530

    Add files via upload

---
+++ Swtch21.java
@@ -0,0 +1,25 @@
+package Ifelsestatements;
+
+import java.util.Scanner;
+
+public class Swtch21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("your number");
+		int num=sc.nextInt();
+		System.out.println(num);
+		System.out.println("select menu");
+		switch(num) {
+		case 1->{
+			System.out.println("dosa");
+		}
+		default->{
+			System.out.println("in valid");
+		}
+		}
+	}
+
+}
