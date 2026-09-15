c11402426ab98a5130c4c46664bf7a2f7383f483
MADDALA DURGAPRASAD <durgaprasadmaddalaa@gmail.com>
2026-09-15 19:39:15 +0530

    Add files via upload

---
+++ Forloop6.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class Forloop6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*for (int i = 10; i >= 1; i--)
+		{
+		    System.out.println(i);
+		*/
+		
+		for(int i=0;i<=50;i=i-1)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ Forloop7.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class Forloop7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i >= 25) {
+			System.out.println(i);
+			i--	;
+		}
+
+	}
+
+}
