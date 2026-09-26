72cea62630c0708cfd51e27ae3c90ef1af9c81d2
MADDALA DURGAPRASAD <durgaprasadmaddalaa@gmail.com>
2026-09-26 10:01:37 +0530

    Add files via upload

---
+++ While1to50.java
@@ -0,0 +1,18 @@
+package Whileloop;
+
+public class While1to50 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=1;
+		
+		while(i<=50)
+		{
+		System.out.println(i);
+		i++;
+		}
+
+	}
+
+}
